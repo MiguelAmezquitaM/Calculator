31f0a94ae5b523a5906fb054f336b6146b32546a
Carlos Castrillón <carlangas.c.m.4@gmail.com>
2023-01-26 16:20:59 -0500

    res.sendFile

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/home', (req, res) => {
-    res.send("hello moto, tumtumtummm tururuuu")
+res.sendFile("index.html", {root:__dirname});
 })
 
 app.listen(8080, () => console.log(`Application is running on http://localhost:${8080}`))
