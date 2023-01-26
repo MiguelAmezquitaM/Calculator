c159ff374b1aa02e2876bc364e7b14d44bff7415
Carlos Castrillón <carlangas.c.m.4@gmail.com>
2023-01-26 14:30:08 -0500

    actu

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/home', (req, res) => {
-    res.send("hello Charle")
+    res.send("hello moto, tumtumtummm tururuuu")
 })
 
 app.listen(8080, () => console.log(`Application is running on http://localhost:${8080}`))
