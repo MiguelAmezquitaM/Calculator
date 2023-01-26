b1892c2b3553f19208826631ffb66085061ee8e0
MiguelAmezquitaM <amezquitamiguel7@gmail.com>
2023-01-26 16:23:15 -0500

    change #00

--- server/main.js
+++ server/main.js
@@ -3,7 +3,15 @@ const express = require('express')
 const app = express()
 
 app.get('/home', (req, res) => {
-    res.send("hello moto, tumtumtummm tururuuu")
+    res.send('Home')
+})
+
+app.get('/calculator', (req, res) => {
+    res.send('The calculator is showed here')
+})
+
+app.use((req, res) => {
+    res.status(404).send("No se ha encontrado")
 })
 
 app.listen(8080, () => console.log(`Application is running on http://localhost:${8080}`))
