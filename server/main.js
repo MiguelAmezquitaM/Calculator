da6bfbbda3414532e4a22f101b0d446fa4b80c9c
MiguelAmezquitaM <amezquitamiguel7@gmail.com>
2023-01-26 13:52:56 -0500

    first commit

---
+++ server/main.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/home', (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(8080, () => console.log(`Application is running on http://localhost:${8080}`))
