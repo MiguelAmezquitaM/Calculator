90591f73811597bb56c8127a187fde091357c7de
MiguelAmezquitaM <amezquitamiguel7@gmail.com>
2023-01-26 19:11:16 -0500

    nombre

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/home', (req, res) => {
-    res.send("hello world")
+    res.send("hello Charles")
 })
 
 app.listen(8080, () => console.log(`Application is running on http://localhost:${8080}`))
